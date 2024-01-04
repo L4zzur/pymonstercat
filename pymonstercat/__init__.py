0ffcae2bcb48ec9abe96fac55522f7b0473456bf
L4zzur <popovnikita01@gmail.com>
2024-01-04 16:09:43 +0200

    add package __init__

---
+++ pymonstercat/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "L4zzur"
+__author_email__ = "popovnikita01@gmail.com"
+__copyright__ = "Copyright 2023, L4zzur"
+__license__ = "MIT"
+__url__ = "https://github.com/L4zzur/pymonstercat/"
+__version__ = "0.1.0"
+
+from .api import MonstercatAPI
