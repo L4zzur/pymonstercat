2a06f9a31d3d3516c88e93103eab46eb13fccba8
L4zzur <popovnikita01@gmail.com>
2024-01-14 12:27:06 +0200

    update test script

--- pymonstercat/scripts/test.py
+++ pymonstercat/scripts/test.py
@@ -5,13 +5,9 @@ from pathlib import Path
 def main():
     from pymonstercat import MonstercatAPI
 
-    EMAIL = os.environ.get("EMAIL")
-    PASSWORD = os.environ.get("PASSWORD")
-
-    path = Path("test.env")
+    path = Path("config_test.yaml")
     print(path.absolute())
     api = MonstercatAPI(config_path=path)
-    api.sign_in_email(EMAIL, PASSWORD)
     # artists = api.get_artists(limit=2, offset=0, search="")
     # for artist in artists:
     #     print(artist.active_years)
