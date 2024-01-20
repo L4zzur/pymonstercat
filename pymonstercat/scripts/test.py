7cb762dde17dd74056cce9761629fcf680cf542b
L4zzur <popovnikita01@gmail.com>
2024-01-20 12:05:17 +0200

    update

--- pymonstercat/scripts/test.py
+++ pymonstercat/scripts/test.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
+from PIL import Image
+from io import BytesIO
+
+import requests
 
 
 def main():
@@ -8,11 +12,15 @@ def main():
     path = Path("config_test.yaml")
     print(path.absolute())
     api = MonstercatAPI(config_path=path)
+    api.sign_in_email()
     # artists = api.get_artists(limit=2, offset=0, search="")
     # for artist in artists:
     #     print(artist.active_years)
-    artist = api.get_artist(artist_uri="fool")
-    print(artist)
+    # artist = api.get_artist(artist_uri="curbi")
+    # print(artist)
+    photo = api.get_artist_photo(artist_uri="curbi")
+    i = Image.open(BytesIO(photo))
+    i.show()
 
 
 if __name__ == "__main__":
