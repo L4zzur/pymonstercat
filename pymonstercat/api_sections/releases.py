551718c15685bc901a7b7a19e699e66897d003f0
Popov Nikita <popovnikita01@gmail.com>
2024-02-20 00:10:57 +0200

    releases: cover art, related releases

--- pymonstercat/api_sections/releases.py
+++ pymonstercat/api_sections/releases.py
@@ -7,6 +7,8 @@ from pymonstercat.models import Release
 class PyMonstercatReleases(PyMonstercatBase):
     RELEASES = PyMonstercatBase.BASE + "releases"
     RELEASE = PyMonstercatBase.BASE + "catalog/release/{id}"
+    RELATED = PyMonstercatBase.BASE + "related-releases/{id}"
+    COVER = "https://www.monstercat.com/release/{id}/cover"
 
     def browse_releases(
         self,
@@ -20,7 +22,9 @@ class PyMonstercatReleases(PyMonstercatBase):
             "offset": offset,
             "search": search,
         }
+
         response = self.get(url=url, params=params)
+
         if response.status_code == 200:
             releases = [
                 Release.from_dict(release)
@@ -41,9 +45,44 @@ class PyMonstercatReleases(PyMonstercatBase):
             params = {"idType": "uuid"}
         else:
             return
-        ic(url, params)
+
         response = self.get(url=url, params=params)
-        ic(response.status_code)
+
         if response.status_code == 200:
             release = Release.from_dict(response.json()["Release"])
             return release
+
+    def get_cover_art(
+        self,
+        catalog_id: str,
+        width: int = 3000,
+        encoding: str = "jpeg",
+    ):
+        params = {
+            "width": width,
+            "encoding": encoding,
+            "url": self.COVER.format(id=catalog_id),
+        }
+
+        response = self.get(url=self.CDX, params=params, follow_redirects=True)
+
+        if response.status_code == 200:
+            return response.url
+
+    # TODO: not working yet
+    def get_related_releases(
+        self,
+        catalog_id: str,
+    ):
+        url = self.RELATED.format(id=catalog_id)
+
+        response = self.get(url=url)
+
+        ic(response.json())
+
+        if response.status_code == 200:
+            releases = [
+                Release.from_dict(release)
+                for release in response.json()["Releases"]["Data"]
+            ]
+            return releases
